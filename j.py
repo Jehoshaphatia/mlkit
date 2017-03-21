3abc49382c2a0fb7b97e83d6d600808e3c19eb58
Jehoshaphat Abu <loveyou725@hotmail.com>
2017-03-22 00:00:00 +0100

    First commit machine learning

---
+++ j.py
@@ -0,0 +1,14 @@
+a=15;
+b=10;
+c=a+b;
+print "The value of c is ",c;
+print "the";
+if a>b:
+    print "the value of a is greater than that of b"
+else:
+    print "the value of b is  greater than the value of a";
+
+#this is a comment
+print"------------------------------------------------------"
+while a<20:     
+    print "yea"
